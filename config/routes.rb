2335b0457795a608d4e13fa161d214062a64e5a3
karlley <karlley.jp@gmail.com>
2022-09-10 04:55:09 +0900

    ユーザ編集後のリダイレクト先を変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'books#index'
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :users, only: %i[index show]
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
