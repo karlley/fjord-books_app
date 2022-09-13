36c5ed66d508205459b21235bfe5c246d6a9f63b
karlley <karlley.jp@gmail.com>
2022-09-13 19:57:19 +0900

    メール認証でのパスワード再設定機能を追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,4 +76,16 @@ Rails.application.configure do
 
   # Default url for mailer
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'smtp.gmail.com',
+    user_name: ENV['GOOGLE_MAIL_ADDRESS'],
+    password: ENV['GOOGLE_APP_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
