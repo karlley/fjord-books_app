2fcf058cc91d23b70f74d14da0e1e5d7e92e20af
karlley <karlley.jp@gmail.com>
2022-09-23 06:25:30 +0900

    自己紹介文のカラム名変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[zipcode address profile])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[zipcode address self_introduction])
   end
 end
