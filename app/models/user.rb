2ca3077a49c497fecf51f1f4496558890b162b52
karlley <karlley.jp@gmail.com>
2022-11-24 08:50:01 +0900

    ユーザーフォロー用アソシエーション設定を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+
+  has_many :active_friendships, class_name: 'Friendship', foreign_key: 'follow_source_id', dependent: :destroy, inverse_of: :follow_source
+  has_many :passive_friendships, class_name: 'Friendship', foreign_key: 'follow_target_id', dependent: :destroy, inverse_of: :follow_target
+  has_many :followings, through: :active_friendships, source: :follow_target
+  has_many :followers, through: :passive_friendships, source: :follow_source
 end
