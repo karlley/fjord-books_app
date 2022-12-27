512aada6d1352e0b14feb7b6f3193a24c043d4f8
karlley <karlley.jp@gmail.com>
2022-12-27 20:54:03 +0900

    ユーザーフォロー機能の多言語化

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,12 +4,12 @@ class FriendshipsController < ApplicationController
   def create
     follow_target = User.find(params[:user_id])
     current_user.follow(follow_target)
-    redirect_to user_path(follow_target), notice: 'フォローしました'
+    redirect_to user_path(follow_target), notice: t('.notice_create')
   end
 
   def destroy
     unfollow_target = User.find(params[:user_id])
     current_user.unfollow(unfollow_target)
-    redirect_to user_path(unfollow_target), notice: 'フォローを解除しました'
+    redirect_to user_path(unfollow_target), notice: t('.notice_destroy')
   end
 end
