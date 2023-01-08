1783770c20fd30c03233e1cb3357afc027c32138
karlley <karlley.jp@gmail.com>
2023-01-08 13:46:28 +0900

    rubocopの指摘点の修正

--- app/controllers/users/followers_controller.rb
+++ app/controllers/users/followers_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Users::FollowersController < ApplicationController
   def index
     @followers = User.find(params[:user_id]).followers.order(:id).page(params[:page])
