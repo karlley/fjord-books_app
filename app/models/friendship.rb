6bfc0b5502c2df2f3854a5b81300cc619af9190f
karlley <karlley.jp@gmail.com>
2023-01-08 09:03:18 +0900

    不要なバリデーションを削除

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,6 +3,4 @@
 class Friendship < ApplicationRecord
   belongs_to :follow_source, class_name: 'User'
   belongs_to :follow_target, class_name: 'User'
-  validates :follow_source_id, presence: true
-  validates :follow_target_id, presence: true
 end
