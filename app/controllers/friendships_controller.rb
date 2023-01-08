e921d33421709057dc7100f0ea8520bbbb6913f0
karlley <karlley.jp@gmail.com>
2023-01-08 12:15:58 +0900

    フォロー/フォロー解除時のフラッシュのキー名変更

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,9 +4,9 @@ class FriendshipsController < ApplicationController
   def create
     follow_target = User.find(params[:user_id])
     if current_user.follow(follow_target)
-      redirect_to user_path(follow_target), notice: t('.notice_create')
+      redirect_to user_path(follow_target), notice: t('.notice')
     else
-      flash.now[:alert] = t('.alert_create')
+      flash.now[:alert] = t('.alert')
       render 'users/show'
     end
   end
@@ -14,9 +14,9 @@ class FriendshipsController < ApplicationController
   def destroy
     unfollow_target = User.find(params[:user_id])
     if current_user.unfollow(unfollow_target)
-      redirect_to user_path(unfollow_target), notice: t('.notice_destroy')
+      redirect_to user_path(unfollow_target), notice: t('.notice')
     else
-      flash.now[:alert] = t('.alert_destroy')
+      flash.now[:alert] = t('.alert')
       render 'users/show'
     end
   end
