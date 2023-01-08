6e7eea3e50b89e1eabcaac20eb561af7a83c3653
karlley <karlley.jp@gmail.com>
2023-01-08 09:06:22 +0900

    フォロー/フォロー解除時の失敗時の処理を追加

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,13 +3,21 @@
 class FriendshipsController < ApplicationController
   def create
     follow_target = User.find(params[:user_id])
-    current_user.follow(follow_target)
-    redirect_to user_path(follow_target), notice: t('.notice_create')
+    if current_user.follow(follow_target)
+      redirect_to user_path(follow_target), notice: t('.notice_create')
+    else
+      flash.now[:alert] = t('.alert_create')
+      render 'users/show'
+    end
   end
 
   def destroy
     unfollow_target = User.find(params[:user_id])
-    current_user.unfollow(unfollow_target)
-    redirect_to user_path(unfollow_target), notice: t('.notice_destroy')
+    if current_user.unfollow(unfollow_target)
+      redirect_to user_path(unfollow_target), notice: t('.notice_destroy')
+    else
+      flash.now[:alert] = t('.alert_destroy')
+      render 'users/show'
+    end
   end
 end
