bfbd163273f406c77d0644bbb93e70af42102f88
karlley <karlley.jp@gmail.com>
2023-01-12 06:18:34 +0900

    follow/unfollowの対象ユーザー取得処理を共通化

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class FriendshipsController < ApplicationController
+  before_action :set_target_user, only: %i[create destroy]
+
   def create
-    target_user = User.find(params[:user_id])
-    if current_user.follow(target_user.id)
-      redirect_to user_path(target_user), notice: t('.notice')
+    if current_user.follow(@target_user.id)
+      redirect_to user_path(@target_user), notice: t('.notice')
     else
       flash.now[:alert] = t('.alert')
       render 'users/show'
@@ -12,12 +13,17 @@ class FriendshipsController < ApplicationController
   end
 
   def destroy
-    target_user = User.find(params[:user_id])
-    if current_user.unfollow(target_user.id)
-      redirect_to user_path(target_user), notice: t('.notice')
+    if current_user.unfollow(@target_user.id)
+      redirect_to user_path(@target_user), notice: t('.notice')
     else
       flash.now[:alert] = t('.alert')
       render 'users/show'
     end
+  end
+
+  private
+
+  def set_target_user
+    @target_user = User.find(params[:user_id])
   end
 end
