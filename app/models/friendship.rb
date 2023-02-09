8dc6db89d021ef250393c3f6db397e742776c5eb
karlley <karlley.jp@gmail.com>
2023-02-10 06:22:21 +0900

    フォロー関係にユニークバリデーションを追加

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,4 +3,6 @@
 class Friendship < ApplicationRecord
   belongs_to :follow_source, class_name: 'User'
   belongs_to :follow_target, class_name: 'User'
+
+  validates :follow_source_id, uniqueness: { scope: :follow_target_id }
 end
