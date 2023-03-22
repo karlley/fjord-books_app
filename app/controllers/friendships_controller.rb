6c8aed89a10546c3ecf89dab8dec1a251828531e
karlley <karlley.jp@gmail.com>
2023-03-23 07:43:57 +0900

    フォロー/アンフォローの例外でエラーメッセージの表示

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,19 +4,17 @@ class FriendshipsController < ApplicationController
   before_action :set_target_user, only: %i[create destroy]
 
   def create
-    if current_user.follow!(@target_user)
-      redirect_to user_path(@target_user), notice: t('.notice')
-    else
-      redirect_to user_path(@target_user), alert: t('.alert')
-    end
+    current_user.follow!(@target_user)
+    redirect_to user_path(@target_user), notice: t('.notice')
+  rescue ActiveRecord::RecordInvalid
+    redirect_to user_path(@target_user), alert: t('.alert')
   end
 
   def destroy
-    if current_user.unfollow!(@target_user)
-      redirect_to user_path(@target_user), notice: t('.notice')
-    else
-      redirect_to user_path(@target_user), alert: t('.alert')
-    end
+    current_user.unfollow!(@target_user)
+    redirect_to user_path(@target_user), notice: t('.notice')
+  rescue ActiveRecord::RecordNotFound
+    redirect_to user_path(@target_user), alert: t('.alert')
   end
 
   private
