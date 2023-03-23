89eb79eb3f111f68ad32aa78394f2336857877ad
karlley <karlley.jp@gmail.com>
2023-03-24 06:18:38 +0900

    フォロー/アンフォロー処理のrescue漏れの修正、共通化

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,19 +2,16 @@
 
 class FriendshipsController < ApplicationController
   before_action :set_target_user, only: %i[create destroy]
+  rescue_from ActiveRecord::RecordInvalid, ActiveRecord::RecordNotFound, ActiveRecord::RecordNotDestroyed, with: :display_follow_unfollow_errer
 
   def create
     current_user.follow!(@target_user)
     redirect_user_page
-  rescue ActiveRecord::RecordInvalid
-    redirect_to user_path(@target_user), alert: t('.alert')
   end
 
   def destroy
     current_user.unfollow!(@target_user)
     redirect_user_page
-  rescue ActiveRecord::RecordNotFound
-    redirect_to user_path(@target_user), alert: t('.alert')
   end
 
   private
@@ -25,5 +22,9 @@ class FriendshipsController < ApplicationController
 
   def redirect_user_page
     redirect_to user_path(@target_user), notice: t('.notice')
+  end
+
+  def display_follow_unfollow_errer
+    redirect_to user_path(@target_user), alert: t('.alert')
   end
 end
