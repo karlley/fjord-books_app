28e69ffa9700ccd2d299f9d979b7a697da90dcfa
karlley <karlley.jp@gmail.com>
2023-03-24 05:17:06 +0900

    フォロー/アンフォロー処理後のリダイレクトを共通化

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -5,14 +5,14 @@ class FriendshipsController < ApplicationController
 
   def create
     current_user.follow!(@target_user)
-    redirect_to user_path(@target_user), notice: t('.notice')
+    redirect_user_page
   rescue ActiveRecord::RecordInvalid
     redirect_to user_path(@target_user), alert: t('.alert')
   end
 
   def destroy
     current_user.unfollow!(@target_user)
-    redirect_to user_path(@target_user), notice: t('.notice')
+    redirect_user_page
   rescue ActiveRecord::RecordNotFound
     redirect_to user_path(@target_user), alert: t('.alert')
   end
@@ -21,5 +21,9 @@ class FriendshipsController < ApplicationController
 
   def set_target_user
     @target_user = User.find(params[:user_id])
+  end
+
+  def redirect_user_page
+    redirect_to user_path(@target_user), notice: t('.notice')
   end
 end
