bffe73602dbb40854493f0944bd5de3a8a37f6f5
karlley <karlley.jp@gmail.com>
2023-03-28 07:25:19 +0900

    フォロー/アンフォロー失敗時の共通処理を削除

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,25 +2,24 @@
 
 class FriendshipsController < ApplicationController
   before_action :set_target_user, only: %i[create destroy]
-  rescue_from ActiveRecord::RecordInvalid, ActiveRecord::RecordNotFound, ActiveRecord::RecordNotDestroyed, with: :display_follow_unfollow_errer
 
   def create
     current_user.follow!(@target_user)
     redirect_to user_path(@target_user), notice: t('.notice')
+  rescue ActiveRecord::RecordInvalid
+    redirect_to user_path(@target_user), alert: t('.alert')
   end
 
   def destroy
     current_user.unfollow!(@target_user)
     redirect_to user_path(@target_user), notice: t('.notice')
+  rescue ActiveRecord::RecordNotFound, ActiveRecord::RecordNotDestroyed
+    redirect_to user_path(@target_user), alert: t('.alert')
   end
 
   private
 
   def set_target_user
     @target_user = User.find(params[:user_id])
-  end
-
-  def display_follow_unfollow_errer
-    redirect_to user_path(@target_user), alert: t('.alert')
   end
 end
