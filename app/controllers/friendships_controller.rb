243540ed4df4a948c34ad9c46054c824b6d10dba
karlley <karlley.jp@gmail.com>
2023-03-28 06:46:22 +0900

    リダイレクト用の共通メソッドを削除

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -6,22 +6,18 @@ class FriendshipsController < ApplicationController
 
   def create
     current_user.follow!(@target_user)
-    redirect_user_page
+    redirect_to user_path(@target_user), notice: t('.notice')
   end
 
   def destroy
     current_user.unfollow!(@target_user)
-    redirect_user_page
+    redirect_to user_path(@target_user), notice: t('.notice')
   end
 
   private
 
   def set_target_user
     @target_user = User.find(params[:user_id])
-  end
-
-  def redirect_user_page
-    redirect_to user_path(@target_user), notice: t('.notice')
   end
 
   def display_follow_unfollow_errer
