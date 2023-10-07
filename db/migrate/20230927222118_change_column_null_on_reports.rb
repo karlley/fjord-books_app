afeb6e328f2777d422a086d0aa7951503e330216
karlley <karlley.jp@gmail.com>
2023-10-07 19:27:02 +0900

    不要なマイグレーションファイルを削除

--- db/migrate/20230927222118_change_column_null_on_reports.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumnNullOnReports < ActiveRecord::Migration[6.1]
-  def change
-    change_column_null :reports, :title, false
-    change_column_null :reports, :content, false
-  end
-end
